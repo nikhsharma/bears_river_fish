d9c2beb0cad30a8243777e519fdf3498cdc13cc0
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-04-03 16:02:23 +0100

    create bear spec

---
+++ specs/bear_spec.rb
@@ -0,0 +1,16 @@
+require('minitest/autorun')
+require('minitest/rg')
+
+require_relative('../bear.rb')
+
+class BearTest < MiniTest::Test
+
+def setup
+  @bear = Bear.new("Yogi", [])
+end
+
+def test_name
+  assert_equal("Yogi", @bear.name)
+end
+
+end
